f38f6beee1ae51ed8fb61ae3b12f33195927e008
Abubakar-Ruwa <109704518+elbewhy@users.noreply.github.com>
2026-07-30 20:43:27 +0100

    Add User model with fields for user data

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,1 +1,20 @@
+from typing import Optional
 
+from sqlmodel import Field
+
+from .base import BaseModel
+
+
+class User(BaseModel, table=True):
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    full_name: str
+
+    username: str
+
+    hashed_password: str
+
+    facility: str
+
+    role: str
