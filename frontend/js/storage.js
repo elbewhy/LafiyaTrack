dd589c86842934ec59ffc2bb0d619ae656e552f5
Abubakar-Ruwa <109704518+elbewhy@users.noreply.github.com>
2026-07-30 21:11:29 +0100

    Create storage.js

---
+++ frontend/js/storage.js
@@ -0,0 +1,29 @@
+const Storage = {
+
+    save(key, data) {
+
+        localStorage.setItem(key, JSON.stringify(data));
+
+    },
+
+    get(key) {
+
+        const data = localStorage.getItem(key);
+
+        return data ? JSON.parse(data) : null;
+
+    },
+
+    remove(key) {
+
+        localStorage.removeItem(key);
+
+    },
+
+    clear() {
+
+        localStorage.clear();
+
+    }
+
+};
